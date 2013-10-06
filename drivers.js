453189207ef68e6daa9ef708a062635070b2207d
Nat Burns <nbaccount@burnskids.com>
2013-10-05 20:20:27 -0400

    Fix reference errors loading drivers.

--- drivers.js
+++ drivers.js
@@ -2,6 +2,6 @@
  * Drivers for various test frameworks.
  */
 module.exports = {
-	jasmine: require('./drivers/jasmine.js').init(defaults),
-	siesta: require('./drivers/siesta.js').init(defaults)
+	jasmine: require('./drivers/jasmine.js'),
+	siesta: require('./drivers/siesta.js')
 };
