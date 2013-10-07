9a093833b8610eb50492dd6954cc5c1243959403
Nat Burns <nbaccount@burnskids.com>
2013-10-07 01:12:39 -0400

    Add more info to Sauce Labs configuration.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,17 +15,49 @@ module.exports = function(grunt) {
 			options: {
 				browsers: [
 					{
+						browserName: 'safari',
+						platform: 'OS X 10.8'
+					},
+					{
+						browserName: 'opera',
+						platform: 'Linux'
+					},
+					{
 						browserName: 'chrome',
 						platform: 'Windows 7'
+					},
+					{
+						browserName: 'firefox',
+						version: '25',
+						platform: 'Windows 7'
+					},
+					{
+						browserName: 'internet explorer',
+						platform: 'Windows 8',
+						version: '10'
+					},
+					{
+						browserName: 'internet explorer',
+						platform: 'Windows 7',
+						version: '9'
+					},
+					{
+						browserName: 'internet explorer',
+						platform: 'Windows XP',
+						version: '8'
 					}
 				],
-				local: grunt.option('local')
+				
+				build: process.env.TRAVIS_JOB_ID,
+				local: grunt.option('local'),
+				concurrency: 3
 			},
 			
 			jasmine: {
 				options: {
 					url: 'http://localhost:9999/jasmine/examples/spec-runner/spec-runner.html',
-					script: saucedriver.jasmine
+					script: saucedriver.jasmine,
+					testname: 'jasmine tests'
 				}
 			}
 		}
